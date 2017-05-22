63df5a2bc6adf52e2cb50e7219c0ff3c72361318
Ian McCallum <ianmccallum@Ians-MacBook-Pro.local>
2017-05-22 22:23:40 +0200

    api/server/routes/customer.js

---
+++ api/server/routes/customer.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+var widgets = [
+	{ id: 1, name: 'TEST Router' },
+	{ id: 2, name: 'Router two' },
+];
+// Get all widgets.
+router.get('/', function(req, res, next) {
+	res.status(200).json(widgets);
+});
+// Get a specific widget.
+router.get('/:id', function(req, res, next) {
+	// Find specific widget.
+	var widget = _(widgets)
+		.find(function(value) {
+			return value.id	== req.params.id;
+		});
+	// If widget was not found set as empty object.
+	if (_.isEmpty(widget)) {
+		widget = {};
+	}
+	res.status(200).json(widget);
+});
+module.exports = router;
